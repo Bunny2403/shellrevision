4a39032b1272ce4389fce5b437a41f8671d766e6
Athreya <mac@macs-MBP.fibrestream.ca>
2021-10-26 00:42:15 -0400

    sample script written

---
+++ scriptpract.sh
@@ -0,0 +1,38 @@
+#! /bin/sh
+
+echo "the present working directory is $PWD"
+
+mkdir delete1
+
+echo "the present working directory is $PWD"
+
+cd delete1
+
+echo "the present working directory is $PWD"
+
+vi abc.docx
+
+echo "the present working directory is $PWD"
+
+chmod 400 abc.docx
+
+cat abc.docx
+
+
+echo "the present working directory is $PWD"
+
+cd ..
+
+echo "the present working directory is $PWD"
+
+cd..
+
+echo "the present working directory is $PWD"
+
+ls
+
+
+
+
+
+
